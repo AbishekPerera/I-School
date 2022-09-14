004dd337578ab265d7f6cff7c07410326acecb5c
IT21013096 <it21013096@my.sliit.lk>
2022-09-15 03:23:55 +0530

    creating project structure

--- app/src/main/java/com/example/i_school/MainActivity.java
+++ app/src/main/java/com/example/i_school/MainActivity.java
@@ -2,15 +2,38 @@ package com.example.i_school;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    Handler handler;
+    Runnable runnable;
+    ImageView img;
+
+    @SuppressLint("Range")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        test
+        img = findViewById(R.id.mainLogo);
+        img.animate().alpha(4000).setDuration(0);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent dsp = new Intent(MainActivity.this,loginReg.class);
+                startActivity(dsp);
+                finish();
+            }
+        },4000);
+
+
     }
 }
