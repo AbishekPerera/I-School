31a80a2ee6cd70c58145f49f29cd19c1184a7171
IT21013096 <it21013096@my.sliit.lk>
2022-09-15 20:35:33 +0530

    add a navigation bar to rocket time table

--- app/src/main/java/com/example/i_school/landingPageNew.java
+++ app/src/main/java/com/example/i_school/landingPageNew.java
@@ -2,7 +2,9 @@ package com.example.i_school;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class landingPageNew extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class landingPageNew extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page_new);
     }
+    public void ClickRocket(View view)
+    {
+        Intent i = new Intent(landingPageNew.this,RocketTimeTable.class);
+        startActivity(i);
+    }
+
 }
