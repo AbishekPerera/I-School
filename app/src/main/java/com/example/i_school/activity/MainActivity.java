422a61cee093f6d93169c3a3e2d6615f9b3b53bf
Sahan Sandeepa <it20611156@my.sliit.lk>
2022-10-30 15:10:32 +0530

    Font_Style_todo

---
+++ app/src/main/java/com/example/i_school/activity/MainActivity.java
@@ -0,0 +1,112 @@
+package com.example.to_do.activity;
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.to_do.R;
+import com.example.to_do.adapter.TaskAdapter;
+import com.example.to_do.bottomSheetFragment.CreateTaskBottomSheetFragment;
+import com.example.to_do.bottomSheetFragment.ShowCalendarViewBottomSheet;
+import com.example.to_do.broadcastReceiver.AlarmBroadcastReceiver;
+import com.example.to_do.database.DatabaseClient;
+import com.example.to_do.model.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//import butterknife for Field and method binding for Android views
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends BaseActivity implements CreateTaskBottomSheetFragment.setRefreshListener {
+
+    //bind views using butter knife
+    @BindView(R.id.taskRecycler)
+    RecyclerView taskRecycler;
+
+    //bind views using butter knife
+    @BindView(R.id.addTask)
+    TextView addTask;
+    TaskAdapter taskAdapter;
+    List<Task> tasks = new ArrayList<>();
+
+    //bind views using butter knife
+    @BindView(R.id.noDataImage)
+    ImageView noDataImage;
+
+    //bind views using butter knife
+    @BindView(R.id.calendar)
+    ImageView calendar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setUpAdapter();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        ComponentName receiver = new ComponentName(this, AlarmBroadcastReceiver.class);
+        PackageManager pm = getPackageManager();
+        pm.setComponentEnabledSetting(receiver, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+        Glide.with(getApplicationContext()).load(R.drawable.first_note).into(noDataImage);
+
+        addTask.setOnClickListener(view -> {
+            CreateTaskBottomSheetFragment createTaskBottomSheetFragment = new CreateTaskBottomSheetFragment();
+            createTaskBottomSheetFragment.setTaskId(0, false, this, MainActivity.this);
+            createTaskBottomSheetFragment.show(getSupportFragmentManager(), createTaskBottomSheetFragment.getTag());
+        });
+
+        getSavedTasks();
+
+        calendar.setOnClickListener(view -> {
+            ShowCalendarViewBottomSheet showCalendarViewBottomSheet = new ShowCalendarViewBottomSheet();
+            showCalendarViewBottomSheet.show(getSupportFragmentManager(), showCalendarViewBottomSheet.getTag());
+        });
+    }
+
+    public void setUpAdapter() {
+        taskAdapter = new TaskAdapter(this, tasks, this);
+        taskRecycler.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        taskRecycler.setAdapter(taskAdapter);
+    }
+
+    private void getSavedTasks() {
+
+        class GetSavedTasks extends AsyncTask<Void, Void, List<Task>> {
+            @Override
+            protected List<Task> doInBackground(Void... voids) {
+                tasks = DatabaseClient
+                        .getInstance(getApplicationContext())
+                        .getAppDatabase()
+                        .dataBaseAction()
+                        .getAllTasksList();
+                return tasks;
+            }
+
+            @Override
+            protected void onPostExecute(List<Task> tasks) {
+                super.onPostExecute(tasks);
+                noDataImage.setVisibility(tasks.isEmpty() ? View.VISIBLE : View.GONE);
+                setUpAdapter();
+            }
+        }
+
+        GetSavedTasks savedTasks = new GetSavedTasks();
+        savedTasks.execute();
+    }
+
+    @Override
+    public void refresh() {
+        getSavedTasks();
+    }
+}
